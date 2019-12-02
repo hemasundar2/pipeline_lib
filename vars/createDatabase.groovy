ba0321c6f450e8c8688c67b47384e41f52386d8d
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 09:32:24 -0600

    Update createDatabase.groovy

--- vars/createDatabase.groovy
+++ vars/createDatabase.groovy
@@ -8,7 +8,7 @@ def call(String user, String password, String database){
           vsql = Sql.newInstance("jdbc:sqlserver://hrsabmssql1dev.nih.gov", user,password, "com.microsoft.sqlserver.jdbc.SQLServerDriver")
     echo 'Hello World 2'
          echo 'create database $database'
-          vsql.execute("create database $database")
+          vsql.execute("create database "+database)
           vsql.close()  
 
 }
