192cb079126bfb2e8b54c83292be355d8a59425c
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 09:21:47 -0600

    Update createDatabase.groovy

--- vars/createDatabase.groovy
+++ vars/createDatabase.groovy
@@ -2,12 +2,12 @@ import groovy.sql.Sql
 
 import java.sql.*
 
-def call(String user, String password){
+def call(String user, String password, String database){
          Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
      
           vsql = Sql.newInstance("jdbc:sqlserver://hrsabmssql1dev.nih.gov", user,password, "com.microsoft.sqlserver.jdbc.SQLServerDriver")
     echo 'Hello World 2'
-          vsql.execute("create database LIQUIBASE_UMS_CORE")
+          vsql.execute("create database $database")
           vsql.close()  
 
 }
