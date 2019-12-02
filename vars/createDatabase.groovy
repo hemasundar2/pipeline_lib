cc4ff8730488294a9be103fb01838b105fa427ce
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 10:51:05 -0600

    Update createDatabase.groovy

--- vars/createDatabase.groovy
+++ vars/createDatabase.groovy
@@ -2,10 +2,10 @@ import groovy.sql.Sql
 
 import java.sql.*
 
-def call(String user, String password, String database){
+def call(String user, String password, String database, String url){
          Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
      
-          vsql = Sql.newInstance("jdbc:sqlserver://hrsabmssql1dev.nih.gov", user,password, "com.microsoft.sqlserver.jdbc.SQLServerDriver")
+          vsql = Sql.newInstance(url, user,password, "com.microsoft.sqlserver.jdbc.SQLServerDriver")
     echo 'Hello World 2'
          echo 'create database $database'
           vsql.execute("create database "+database)
